62755d09f6d58413820e6dddc367ea539d9fe237
ToniK4 <toni.kneze4@gmail.com>
2021-09-07 10:25:30 +0200

    Console Writeline writes out information

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -11,10 +11,23 @@ namespace ClassRoom
         static void Main(string[] args)
         {
             ClassRoom Classroom = new ClassRoom();
+            Classroom.ClassList = new List<Student>();
+
             Classroom.ClassName = "3Q";
             Classroom.SemesterStart = new DateTime(2019, 08, 26);
             Classroom.ClassList.Add(new Student("Toni", 09, 03));
             Classroom.ClassList.Add(new Student("Gabor", 11, 03));
+
+            Console.WriteLine("Class Name: " + Classroom.ClassName);
+            Console.WriteLine("Semester Start: " + Classroom.SemesterStart);
+            
+            foreach (Student Student in Classroom.ClassList)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Student Name: " + Student.Name);
+                Console.WriteLine("Student Birth Month: " + Student.BirthMonth);
+                Console.WriteLine("Student Birthday: " + Student.Birthday);
+            }
         }
     }
 }
